758a84f5804025b749b8ee32c68f027cb579dbf5
neeti-yaav <neetiy32@gmail.com>
2025-11-11 00:20:06 +0530

    11Nov2025:00:20

--- rps-reactor game/app.js
+++ rps-reactor game/app.js
@@ -2,6 +2,11 @@ let userScore = 0;
 let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+
+const userScorePara = document.querySelector("#user-score");
+const CompScorePara = document.querySelector("#comp-score");
+
 
 const genCompChoice = () => {
     const options = ["rock", "paper","scissors"];
@@ -20,11 +25,16 @@ const  drawGame = () => {
 
 const showWinner = (userWin ,userChoice, compChoice) => {
     if(userWin){
+        userScore++;
+        userScorePara.innerText = userScore;
         console.log("You Win!");
         msg.innerText = `You Win! Your ${userChoice} beats ${compChoice}`;
         msg.style.backgroundColor = "green";
 
     }else{
+        compScore++;
+        CompScorePara.innerText = compScore;
+
         console.log("You Lose!");
         msg.innerText = `You Lose.${compChoice} beats Your ${userChoice}`;
         msg.style.backgroundColor = "red";
